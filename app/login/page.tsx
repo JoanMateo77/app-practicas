574008c2c2a01f0dc9250230068a9a4f5f30d008
JoanMateo77 <joanmateo1102@gmail.com>
2025-07-07 17:19:39 -0500

    Style(login):mejoras de aspecto solamente visual

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,13 @@
 export default function LoginPage(){
     return(
         <main className="flex min-h-screen items-center justify-center
-                            bg gradient-to-br from-indigo-100 via-white to-purple-100
+                            bg-gradient-to-br from-indigo-100 via-white to-purple-100
                             px-4">
 
-            <form className="w-full max-w-md space-y-6 rounded-2x1 bg-white p-8px
-                                shadow-x1 shadow-indigo-100 ring-1 ring-gray-200" onSubmit={handleLogin}>
+            <form className="w-full max-w-md space-y-6 rounded-2xl bg-white p-8
+                                shadow-xl shadow-indigo-100 ring-1 ring-gray-200" onSubmit={handleLogin}>
 
-                <h1 className="text-3x1 font bold text-center text-gray-800 ">Iniciar Sesion</h1>
+                <h1 className="text-3xl font-bold text-center text-gray-800 ">Iniciar Sesion</h1>
                 <div className="space-y-4">
                     <label className="block">
                         <span className="block text-sm font-medium text-gray-700">Correo Electronico</span>
@@ -18,7 +18,7 @@ export default function LoginPage(){
                 <input type="email" 
                 required
                 placeholder="tuCorreo@gmial.com"
-                className="mt-1 w-ful rounded-lg border-gray-300 shadow-sm focus:border-indigo-500
+                className="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500
                             focus:ring-indigo-500" />
                             </label>
                 
